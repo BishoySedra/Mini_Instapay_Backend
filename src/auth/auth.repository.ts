c77ff4710dd1c50fb1fb160eeaf8cabd21602a9d
Bishoy Sedra <bishosedra0@gmail.com>
2025-08-04 08:26:32 +0300

    Added some changes to solve issue of internal server error of signup endpoint!

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { Prisma, User } from '@prisma/client';
+import { RegisterPayloadDTO } from './dto/register.dto';
 
 @Injectable()
 export class AuthRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+  async createUser(data: RegisterPayloadDTO): Promise<User> {
+
+    const { email, password, name, phone, address } = data;
+
     return this.prisma.user.create({
-      data,
+      data: {
+        email,
+        password, // Ensure this is hashed before saving
+        name,
+        phone,
+        address,
+      }
     });
+
   }
 
   async findUserByEmail(email: string): Promise<User | null> {
